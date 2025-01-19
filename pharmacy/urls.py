9763863d0926c9ff9fc3f5b98251cb3211f26d17
ngueagho <ngueaghotiodongrobertolandry@gmail.com>
2025-01-19 14:48:13 +0100

    ROBERTO : 19-01-024

--- pharmacy/urls.py
+++ pharmacy/urls.py
@@ -104,10 +104,79 @@ urlpatterns=[
     path('reset/<uidb64>/<token>/',auth_views.PasswordResetConfirmView
     .as_view(template_name="password_reset_form.html"),name="password_reset_confirm"),
 
-
-
-    
-
    path('reset_password_complete/',auth_views.PasswordResetCompleteView
     .as_view(template_name="password_reset_done.html"),name="password_reset_complete"),
-]
+] 
+
+
+# from django.urls import path
+# from . import HODViews, pharmacistViews, DoctorViews, views, patient_view, clerkViews
+# from django.contrib.auth import views as auth_views
+
+# urlpatterns = [
+#     # Admin (HOD) Views
+#     path('', HODViews.adminDashboard, name='admin_dashboard'),
+#     path('admin_user/patient_form/', HODViews.createPatient, name='patient_form'),
+#     path('admin_user/all_patients/', HODViews.allPatients, name='all_patients'),
+#     path('login/', views.loginPage, name='login'),
+#     path('logout/', views.logoutUser, name='logout'),
+
+#     path('admin_user/add_pharmacist/', HODViews.createPharmacist, name='add_pharmacist'),
+#     path('admin_user/manage_pharmacist/', HODViews.managePharmacist, name='manage_pharmacist'),
+#     path('admin_user/add_doctor/', HODViews.createDoctor, name='add_doctor'),
+#     path('admin_user/manage_doctor/', HODViews.manageDoctor, name='manage_doctor'),
+#     path('admin_user/add_pharmacyClerk/', HODViews.createPharmacyClerk, name='add_pharmacyClerk'),
+#     path('admin_user/manage_pharmacyClerk/', HODViews.managePharmacyClerk, name='manage_pharmacyClerk'),
+#     path('admin_user/add_stock/', HODViews.addStock, name='add_stock'),
+#     path('admin_user/add_category/', HODViews.addCategory, name='add_category'),
+#     path('admin_user/manage_stock/', HODViews.manageStock, name='manage_stock'),
+#     path('admin_user/prescribe_drug/', HODViews.addPrescription, name='prescribe'),
+#     path('admin_user/edit_patient/<int:patient_id>/', HODViews.editPatient, name='edit_patient'),
+#     path('admin_user/delete_patient/<str:pk>/', HODViews.confirmDelete, name='delete_patient'),
+#     path('admin_user/patient_personalRecords/<str:pk>/', HODViews.patient_personalRecords, name='patient_record'),
+#     path('admin_user/delete_prescription/<str:pk>/', HODViews.deletePrescription, name='delete_prescription'),
+#     path('admin_user/hod_profile/', HODViews.hodProfile, name='hod_profile'),
+#     path('admin_user/edit_hod_profile/', HODViews.editAdmin, name='edit-admin'),
+#     path('admin_user/delete_drug/<str:pk>/', HODViews.deleteDrug, name='delete_drug'),
+
+#     # Pharmacist Views
+#     path('pharmacist_home/', pharmacistViews.pharmacistHome, name='pharmacist_home'),
+#     path('pharmacist_manage_patients/', pharmacistViews.managePatientsPharmacist, name='manage_patient_pharmacist'),
+#     path('manage_disp/<str:pk>/', pharmacistViews.manageDispense, name='pharmacist_disp'),
+#     path('staff_profile/', pharmacistViews.userProfile, name='pharmacist_profile'),
+#     path('manage_stock2/', pharmacistViews.manageStock, name='manage_stock2'),
+#     path('manage_prescrip/', pharmacistViews.managePrescription, name='pharmacist_prescription'),
+#     path('pharmacist_user/drug_details/<str:pk>/', pharmacistViews.drugDetails, name="drug_detail2"),
+
+#     # Doctor Views
+#     path('doctor_home/', DoctorViews.doctorHome, name='doctor_home'),
+#     path('manage_patients/', DoctorViews.managePatients, name='manage_patient_doctor'),
+#     path('doctor_prescribe_drug/<str:pk>/', DoctorViews.addConsultation, name='doctor_prescribe2'),
+#     path('patient_personalDetails/<str:pk>/', DoctorViews.patient_personalDetails, name='patient_record_doctor'),
+#     path('manage_prescription_doctor/', DoctorViews.managePrescription, name='manage_prescrip_doctor'),
+#     path('doctor_prescribe_delete/<str:pk>/', DoctorViews.deletePrescription, name='doctor_prescrip_delete'),
+#     path('doctor_prescribe_edit/<str:pk>/', DoctorViews.editPrescription, name='doctor_prescrip_edit'),
+
+#     # Patient Views
+#     path('patient_profile/', patient_view.patientProfile, name='patient_profile'),
+#     path('patient_home/', patient_view.patientHome, name='patient_home'),
+#     path('patient_feedback/', patient_view.patient_feedback, name='patient_feedback'),
+#     path('staff_feedback_save/', patient_view.patient_feedback_save, name="patient_feedback_save"),
+#     path('delete_patient_feedback2/<str:pk>/', patient_view.Patientdeletefeedback, name="delete_fed2"),
+#     path('delete_dispen/', patient_view.myPrescriptionDelete, name='taken_delete'),
+
+#     # Receptionist (Clerk) Views
+#     path('receptionist_profile/', clerkViews.receptionistProfile, name='clerk_profile'),
+#     path('receptionist_home/', clerkViews.clerkHome, name='clerk_home'),
+#     path('receptionist/patient_form/', clerkViews.createPatient, name='patient_form2'),
+#     path('receptionist/all_patients/', clerkViews.allPatients, name='all_patients2'),
+#     path('receptionist/edit_patient/<int:patient_id>/', clerkViews.editPatient, name='edit_patient_clerk'),
+#     path('receptionist/patient_personalRecords/<str:pk>/', clerkViews.patient_personalRecords, name='patient_record_clerk'),
+#     path('receptionist/delete_patient/<str:pk>/', clerkViews.confirmDelete, name='delete_patient_clerk'),
+
+#     # Password Reset
+#     path('reset_password/', auth_views.PasswordResetView.as_view(template_name="password_reset.html"), name="reset_password"),
+#     path('reset_password_sent/', auth_views.PasswordResetDoneView.as_view(template_name="password_reset_sent.html"), name="password_reset_done"),
+#     path('reset/<uidb64>/<token>/', auth_views.PasswordResetConfirmView.as_view(template_name="password_reset_form.html"), name="password_reset_confirm"),
+#     path('reset_password_complete/', auth_views.PasswordResetCompleteView.as_view(template_name="password_reset_done.html"), name="password_reset_complete"),
+# ]
